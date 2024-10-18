1437bf24a705ab57b98d4603dfaaf7a353e4a552
muhamademiralfani <muhamademiralfani@gmail.com>
2024-10-18 18:22:21 +0700

    Melakukan perubahan dengan menambahkan javascript dan juga beberap styles tambahan

---
+++ script.js
@@ -0,0 +1,53 @@
+document.getElementById('postForm').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const formData = {
+    to: 'emiralcimsat123@gmail.com',
+    name: document.querySelector('input[name="userId"]').value,
+    subject: document.querySelector('input[name="title"]').value,
+    text: document.querySelector('textarea[name="body"]').value,
+  };
+
+  fetch('https://lumoshive-academy-email-api.vercel.app/send-email', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': 'RJS1-202413',
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById('postForm').reset();
+      Swal.fire({
+        title: 'Success!',
+        text: 'Your message has been sent.',
+        confirmButtonColor: '#366cd8',
+      });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      Swal.fire({
+        title: 'Error!',
+        text: 'Failed to send message. Please try again.',
+        icon: 'error',
+        confirmButtonColor: '#366cd8',
+        confirmButtonText: 'Try Again',
+        // Custom style
+        customClass: {
+          confirmButton: 'custom-confirm-button',
+          popup: 'custom-popup',
+        },
+      });
+    });
+});
+
+// Ambil elemen yang diperlukan
+const menuBtn = document.getElementById('menu-btn');
+const navLinks = document.getElementById('nav-links');
+
+// Tambahkan event listener untuk hamburger menu
+menuBtn.addEventListener('click', () => {
+  // Toggle class 'open' untuk nav links
+  navLinks.classList.toggle('open');
+});
